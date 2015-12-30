2141477c4cb09c4668f6925ac07de1a657c1cc89
mzp <mzpppp@gmail.com>
2015-12-30 17:41:46 +0900

    add sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ var gulp = require('gulp');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var fs = require("fs");
+var source     = require( 'vinyl-source-stream' );
 
 gulp.task('build', function() {
-  browserify({ entries: ['app/js/main.js'] })
+  browserify({ entries: ['app/js/main.js'], debug: true })
     .transform(babelify, {
       presets: ['stage-0', 'es2015', 'react']
     })
     .bundle()
-    .pipe(fs.createWriteStream("bundle/main.js"));
+    .pipe( source( 'main.js' ) )
+    .pipe( gulp.dest( './bundle' ));
 });
 
 gulp.task('default', ['build']);
