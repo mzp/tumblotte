9b24601c64a4ce70f72fc5103ef0d24b13a9a9f8
mzp <mzpppp@gmail.com>
2015-12-30 18:48:49 +0900

    add electron-package

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,31 @@ var gulp = require('gulp');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var fs = require("fs");
-var source     = require( 'vinyl-source-stream' );
+var source = require( 'vinyl-source-stream' );
+var packager = require('electron-packager');
 
 gulp.task('build', function() {
-  browserify({ entries: ['app/js/main.js'], debug: true })
+  browserify({ entries: ['src/main.js'], debug: true })
     .transform(babelify, {
       presets: ['stage-0', 'es2015', 'react']
     })
     .bundle()
-    .pipe( source( 'main.js' ) )
-    .pipe( gulp.dest( './app/bundle' ));
+    .pipe( source( 'bundle.js' ) )
+    .pipe( gulp.dest( './app/' ));
+});
+
+gulp.task('package', ['build'], function(done) {
+ packager({
+   dir: 'app',
+   out: 'package',
+   name: 'Tumblr',
+   arch: 'x64',
+   platform: 'darwin',
+   version: '0.36.2',
+   overwrite: true
+ }, function (err, path) {
+   done();
+ });
 });
 
 gulp.task('default', ['build']);
