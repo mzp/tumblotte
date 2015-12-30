a6395aa576ede9feadff4b2c6fa1c80af19ce9d9
mzp <mzpppp@gmail.com>
2015-12-30 16:23:36 +0900

    add react/react-dom

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,12 @@
-let msg = 'hello';
-console.log(msg);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+    render() {
+      return <div>Hello {this.props.name}</div>;
+    }
+}
+
+let mountNode = document.getElementById('root');
+
+ReactDOM.render(<HelloMessage name='John' />, mountNode);
