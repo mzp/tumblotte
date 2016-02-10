ebb97b5f920c82fd541fbc9a5be2a92a1521fd81
mzp <mzpppp@gmail.com>
2016-02-11 08:49:48 +0900

    :bug: open link in external window at preview window

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import marked from 'marked';
+import { Renderer } from 'marked';
+
+// Don't open external link in electron window.
+const renderer = new Renderer();
+renderer.link = (href, title, text) => {
+  return `<a href="#" title=${title} link=${href} >${text}</a>`
+};
 
 export default class Preview extends React.Component {
   rawMarkup() {
     const { post } = this.props;
-
     if(post) {
-      const rawMarkup = marked(post.body, {sanitize: true});
+      const rawMarkup = marked(post.body, { sanitize: true, renderer });
       return { __html: rawMarkup };
     } else {
       return { __html: '' };
+    }
+  }
+
+  click(e) {
+    if (e.target.tagName == 'A') {
+      const { link } = e.target.attributes;
+      global.require('shell').openExternal(link.value);
     }
   }
 
@@ -18,7 +31,7 @@ export default class Preview extends React.Component {
 
     return <div id="preview" className="pure-u-1-2 preview">
       <h1 className="preview__title">{post && post.title}</h1>
-      <div className="preview__body" dangerouslySetInnerHTML={this.rawMarkup()} />
+      <div className="preview__body" dangerouslySetInnerHTML={this.rawMarkup()} onClick={::this.click}/>
     </div>
   }
 }
